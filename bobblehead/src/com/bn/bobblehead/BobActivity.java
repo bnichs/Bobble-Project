cbb2fcc8a617997c628e80e7bc5551b1e5891dbb
Alex Wiles <amw4292@gmail.com>
2012-12-09 12:55:28 -0500

    Got Rid of a bunch of unused code
    perticle class and system is all gone,  now head is being enlarged.

--- bobblehead/src/com/bn/bobblehead/BobActivity.java
+++ bobblehead/src/com/bn/bobblehead/BobActivity.java
@@ -167,7 +167,6 @@ public class BobActivity extends Activity {
         private long mCpuTimeStamp;
         private float mHorizontalBound;
         private float mVerticalBound;
-        private final ParticleSystem mParticleSystem = new ParticleSystem();
         private float angle=0;
         
         private Face face;
@@ -191,6 +190,8 @@ public class BobActivity extends Activity {
             private double springK = 0.000003;
             private double dampingK = 0.000035;
         	
+            private float width, height;
+            
         	public Face(RectF box){
         		
         		rec=box;
@@ -204,8 +205,8 @@ public class BobActivity extends Activity {
                 
                 
                 
-                float width=right-left;
-                float height=bottom-top;
+                width=right-left;
+                height=bottom-top;
                 
                 left=left-width/4f;
                 right=right+width/4f;
@@ -214,8 +215,7 @@ public class BobActivity extends Activity {
                 bottom=bottom+height/5f;
                 
                 boxRec=new RectF(left,top,right,bottom);
-                
-                //face=Bitmap.createBitmap(backg,(int)rec.left,(int)rec.top,(int)rec.width(),(int)rec.height());
+
                
                 face=BitmapFactory.decodeFile(HomeScreen.faceFil.toString());
                 
@@ -226,97 +226,12 @@ public class BobActivity extends Activity {
         	
         	public void update(float sx, float sy, long timestamp){
         		
-        		
-        		//mPosX+=.00001f;
-        		
-        		final long t = timestamp;
-                if (mLastT != 0) {
-                    final float dT = (float) (t - mLastT) * (1.0f / 1000000000.0f);
-                    if (mLastDeltaT != 0) {
-                        final float dTC = dT / mLastDeltaT;
-                        this.computePhysics(sx, sy, dT, dTC);
-                        
-                    }
-                    mLastDeltaT = dT;
-                }
-                mLastT = t;
-                if (mPosX != rec.left || mPosY != rec.top){
-                	final float width=rec.right-rec.left;
-                	final float height=rec.bottom-rec.top;
-                	rec.left=mPosX;
-                	rec.right=mPosX+width;
-                	rec.top=mPosY;
-                	rec.bottom=mPosY+height;
-                }
-               this.resolveCollisionsWithBounds();
+        	
                 
                 
         	}
         	
-        	public void resolveCollisionsWithBounds(){
-        		float bLeft=boxRec.left;
-        		float bRight=boxRec.right;
-        		float bTop=boxRec.top;
-        		float bBottom=boxRec.bottom;
-        		
-        		float left=rec.left;
-        		float right=rec.right;
-        		float top=rec.top;
-        		float bottom=rec.bottom;
-        		
-        		if (left<bLeft){
-        			left=bLeft;
-        			right=bLeft+right-left;
-        			}
-        		else if (right>bRight){
-        			right=bRight;
-        			left=bRight-right+left;
-        			}
-        		if (top<bTop){top=bTop;}
-        		else if (bottom>bBottom){bottom=bBottom;}
-        		
-        		rec=new RectF(left,top,right,bottom);
-        		
-        	}
         	
-        	 /*public void computePhysics(float sx, float sy, float dT, float dTC) {
-                 // Force of gravity applied to our virtual object
-                 final float m = 1000.0f; // mass of our virtual object
-                 final float gx = -sx * m;
-                 final float gy = -sy * m;
-
-                 /*
-                  * �F = mA <=> A = �F / m We could simplify the code by
-                  * completely eliminating "m" (the mass) from all the equations,
-                  * but it would hide the concepts from this sample code.
-                 
-                 final float invm = 1.0f / m;
-                 final float ax = gx * invm;
-                 final float ay = gy * invm;
- */
-                 /*
-                  * Time-corrected Verlet integration The position Verlet
-                  * integrator is defined as x(t+�t) = x(t) + x(t) - x(t-�t) +
-                  * a(t)�t�2 However, the above equation doesn't handle variable
-                  * �t very well, a time-corrected version is needed: x(t+�t) =
-                  * x(t) + (x(t) - x(t-�t)) * (�t/�t_prev) + a(t)�t�2 We also add
-                  * a simple friction term (f) to the equation: x(t+�t) = x(t) +
-                  * (1-f) * (x(t) - x(t-�t)) * (�t/�t_prev) + a(t)�t�2
-                  
-                 final float dTdT = dT * dT;
-                 final float x = mPosX + mOneMinusFriction * dTC * (mPosX - mLastPosX) + mAccelX
-                         * dTdT;
-                 final float y = mPosY + mOneMinusFriction * dTC * (mPosY - mLastPosY) + mAccelY
-                         * dTdT;
-                 mLastPosX = mPosX;
-                 mLastPosY = mPosY;
-                 mPosX = x;
-                 mPosY = y;
-                 mAccelX = ax;
-                 mAccelY = ay;
-             }*/
-           
-        	 
         	 public void computePhysics(float sx, float sy, float dT, float dTC) {
              	float vx = (float) (sx * (-springK - mAccelX * dampingK));
              	float vy = (float) (sy * (-springK - mAccelY * dampingK));
@@ -339,230 +254,10 @@ public class BobActivity extends Activity {
         	}
 
         }
-        
-        
-        
-        /*
-         * Each of our particle holds its previous and current position, its
-         * acceleration. for added realism each particle has its own friction
-         * coefficient.
-         */
-        class Particle {
-            private float mPosX;
-            private float mPosY;
-            private float mAccelX;
-            private float mAccelY;
-            private float mLastPosX;
-            private float mLastPosY;
-            private float mOneMinusFriction;
- 
-            
-            private Bitmap face;
-            
-
-            Particle() {
-                // make each particle a bit different by randomizing its
-                // coefficient of friction
-                final float r = ((float) Math.random() - 0.5f) * 0.2f;
-                mOneMinusFriction = 1.0f - sFriction + r;
-                //face=bm;
-                
-                
-            }
-
-            
-            
-            public void computePhysics(float sx, float sy, float dT, float dTC) {
-                // Force of gravity applied to our virtual object
-                final float m = 1000.0f; // mass of our virtual object
-                final float gx = -sx * m;
-                final float gy = -sy * m;
-
-                /*
-                 * �F = mA <=> A = �F / m We could simplify the code by
-                 * completely eliminating "m" (the mass) from all the equations,
-                 * but it would hide the concepts from this sample code.
-                 */
-                final float invm = 1.0f / m;
-                final float ax = gx * invm;
-                final float ay = gy * invm;
-
-                /*
-                 * Time-corrected Verlet integration The position Verlet
-                 * integrator is defined as x(t+�t) = x(t) + x(t) - x(t-�t) +
-                 * a(t)�t�2 However, the above equation doesn't handle variable
-                 * �t very well, a time-corrected version is needed: x(t+�t) =
-                 * x(t) + (x(t) - x(t-�t)) * (�t/�t_prev) + a(t)�t�2 We also add
-                 * a simple friction term (f) to the equation: x(t+�t) = x(t) +
-                 * (1-f) * (x(t) - x(t-�t)) * (�t/�t_prev) + a(t)�t�2
-                 */
-                final float dTdT = dT * dT;
-                final float x = mPosX + mOneMinusFriction * dTC * (mPosX - mLastPosX) + mAccelX
-                        * dTdT;
-                final float y = mPosY + mOneMinusFriction * dTC * (mPosY - mLastPosY) + mAccelY
-                        * dTdT;
-                mLastPosX = mPosX;
-                mLastPosY = mPosY;
-                mPosX = x;
-                mPosY = y;
-                mAccelX = ax;
-                mAccelY = ay;
-            }
-           /*
-            *  public void computePhysics(float sx, float sy, float dT, float dTC) {
-            	float vx = (float) (sx * (-springK - mAccelX * dampingK));
-            	float vy = (float) (sy * (-springK - mAccelY * dampingK));
-            	mAccelX += vx * dT;
-            	mAccelY += vy * dT;
-            	
-            	mLastPosX = mPosX;
-            	mLastPosY = mPosY;
-            	mPosX = mAccelX * dT;
-            	mPosY = mAccelY * dT;
-	     }*/
-
-            /*
-             * Resolving constraints and collisions with the Verlet integrator
-             * can be very simple, we simply need to move a colliding or
-             * constrained particle in such way that the constraint is
-             * satisfied.
-             */
-            public void resolveCollisionWithBounds() {
-                final float xmax = mHorizontalBound;
-                //System.out.println(xmax);
-                //System.out.println(mHorizontalBound*mMetersToPixelsX+":"+mVerticalBound*mMetersToPixelsY);
-                
-                final float ymax = mVerticalBound;
-                final float x = mPosX;
-                final float y = mPosY;
-                if (x > xmax) {
-                    mPosX = xmax;
-                } else if (x < -xmax) {
-                    mPosX = -xmax;
-                }
-                if (y > ymax) {
-                    mPosY = ymax;
-                } else if (y < -ymax) {
-                    mPosY = -ymax;
-                }
-            }
-        }
-
-        /*
-         * A particle system is just a collection of particles
-         */
-        class ParticleSystem {
-            static final int NUM_PARTICLES = 1;
-            private Particle mBalls[] = new Particle[NUM_PARTICLES];
-
-            ParticleSystem() {
-                /*
-                 * Initially our particles have no speed or acceleration
-                 */
-                for (int i = 0; i < mBalls.length; i++) {
-                    mBalls[i] = new Particle();
-                }
-            }
-
-            /*
-             * Update the position of each particle in the system using the
-             * Verlet integrator.
-             */
-            private void updatePositions(float sx, float sy, long timestamp) {
-                final long t = timestamp;
-                if (mLastT != 0) {
-                    final float dT = (float) (t - mLastT) * (1.0f / 1000000000.0f);
-                    if (mLastDeltaT != 0) {
-                        final float dTC = dT / mLastDeltaT;
-                        final int count = mBalls.length;
-                        for (int i = 0; i < count; i++) {
-                            Particle ball = mBalls[i];
-                            ball.computePhysics(sx, sy, dT, dTC);
-                        }
-                    }
-                    mLastDeltaT = dT;
-                }
-                mLastT = t;
-            }
-
-            /*
-             * Performs one iteration of the simulation. First updating the
-             * position of all the particles and resolving the constraints and
-             * collisions.
-             */
-            public void update(float sx, float sy, long now) {
-                // update the system's positions
-                updatePositions(sx, sy, now);
-
-                // We do no more than a limited number of iterations
-                final int NUM_MAX_ITERATIONS = 10;
-
-                /*
-                 * Resolve collisions, each particle is tested against every
-                 * other particle for collision. If a collision is detected the
-                 * particle is moved away using a virtual spring of infinite
-                 * stiffness.
-                 */
-                boolean more = true;
-                final int count = mBalls.length;
-                for (int k = 0; k < NUM_MAX_ITERATIONS && more; k++) {
-                    more = false;
-                    for (int i = 0; i < count; i++) {
-                        Particle curr = mBalls[i];
-                        for (int j = i + 1; j < count; j++) {
-                            Particle ball = mBalls[j];
-                            float dx = ball.mPosX - curr.mPosX;
-                            float dy = ball.mPosY - curr.mPosY;
-                            float dd = dx * dx + dy * dy;
-                            // Check for collisions
-                            if (dd <= sBallDiameter2) {
-                                /*
-                                 * add a little bit of entropy, after nothing is
-                                 * perfect in the universe.
-                                 */
-                                dx += ((float) Math.random() - 0.5f) * 0.0001f;
-                                dy += ((float) Math.random() - 0.5f) * 0.0001f;
-                                dd = dx * dx + dy * dy;
-                                // simulate the spring
-                                final float d = (float) Math.sqrt(dd);
-                                final float c = (0.5f * (sBallDiameter - d)) / d;
-                                curr.mPosX -= dx * c;
-                                curr.mPosY -= dy * c;
-                                ball.mPosX += dx * c;
-                                ball.mPosY += dy * c;
-                                more = true;
-                            }
-                        }
-                        /*
-                         * Finally make sure the particle doesn't intersects
-                         * with the walls.
-                         */
-                        curr.resolveCollisionWithBounds();
-                    }
-                }
-            }
-
-            public int getParticleCount() {
-                return mBalls.length;
-            }
-
-            public float getPosX(int i) {
-                return mBalls[i].mPosX;
-            }
-
-            public float getPosY(int i) {
-                return mBalls[i].mPosY;
-            }
-        }
+       
 
         public void startSimulation() {
-            /*
-             * It is not necessary to get accelerometer events at a very high
-             * rate, by using a slower rate (SENSOR_DELAY_UI), we get an
-             * automatic low-pass filter, which "extracts" the gravity component
-             * of the acceleration. As an added benefit, we use less power and
-             * CPU resources.
-             */
+
             mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
             mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_GAME);
         }
@@ -654,6 +349,7 @@ public class BobActivity extends Activity {
         
         private Paint p=new Paint();
 
+
         @Override
         protected void onDraw(Canvas canvas) {
 
@@ -668,7 +364,7 @@ public class BobActivity extends Activity {
              * data and present time.
              */
 
-            final ParticleSystem particleSystem = mParticleSystem;
+
             final long now = mSensorTimeStamp + (System.nanoTime() - mCpuTimeStamp);
             
             
@@ -684,27 +380,11 @@ public class BobActivity extends Activity {
             final float xs = mMetersToPixelsX;
             final float ys = mMetersToPixelsY;
             final Bitmap bitmap = mBitmap;
-            final int count = particleSystem.getParticleCount();
-            for (int i = 0; i < count; i++) {
-                /*
-                 * We transform the canvas so that the coordinate system matches
-                 * the sensors coordinate system with the origin in the center
-                 * of the screen and the unit is the meter.
-                 */
-
-                final float x = xc + particleSystem.getPosX(i) * xs;
-                final float y = yc - particleSystem.getPosY(i) * ys;
-                
-                
-                //System.out.println(angle);
-                //canvas.drawBitmap(bitmap, x, y, null);
-            }
             
             //face.face.setDensity(300);
             
-            
-            
-            canvas.drawBitmap(face.face,null,face.rec, null);
+           
+            canvas.drawBitmap(face.face,null,face.boxRec, null);
             
             //System.out.println(face.face.getWidth()+":"+face.face.getHeight());
             
