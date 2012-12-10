99308f56a81a481d2462a9e623b34a938cd8632c
Alex Wiles <amw4292@gmail.com>
2012-12-10 00:51:27 -0500

    working on bobble head

--- bobblehead/jni/imageprocessing.c
+++ bobblehead/jni/imageprocessing.c
@@ -22,32 +22,67 @@ static int rgb_clamp(int value) {
 }
 
 static void brightness(AndroidBitmapInfo* info, void* pixels, float brightnessValue){
-	int xx, yy, red, green, blue;
 	uint32_t* line;
-    
-	for(yy = 0; yy < info->height; yy++){
-        line = (uint32_t*)pixels;
-        for(xx =0; xx < info->width; xx++){
+    float h = info->height;
+    float w = info->width;
+    // for each row
+    int y = 0;
+    for (y;y<h;y++){
+        // normalize y coordinate to -1 ... 1
+        double ny = ((2*y)/h)-1;
+        // pre calculate ny*ny
+        double ny2 = ny*ny;
+        // for each column
+        int x = 0;
+        for (x;x<w;x++)
+        {
+            // normalize x coordinate to -1 ... 1
+            double nx = ((2*x)/w)-1;
+            // pre calculate nx*nx
             
-            //extract the RGB values from the pixel
-            red = (int) ((line[xx] & 0x00FF0000) >> 16);
-            green = (int)((line[xx] & 0x0000FF00) >> 8);
-            blue = (int) (line[xx] & 0x00000FF );
+            double nx2 = nx*nx;
             
-            //manipulate each value
-            red = rgb_clamp((int)(red * brightnessValue));
-            green = rgb_clamp((int)(green * brightnessValue));
-            blue = rgb_clamp((int)(blue * brightnessValue));
             
-            // set the new pixel back in
-            line[xx] =
-            ((red << 16) & 0x00FF0000) |
-            ((green << 8) & 0x0000FF00) |
-            (blue & 0x000000FF);
+            // calculate distance from center (0,0)
+            // this will include circle or ellipse shape portion
+            // of the image, depending on image dimensions
+            // you can experiment with images with different dimensions
+            double r = sqrt(nx2+ny2);
+            // discard pixels outside from circle!
+            if (0.0<=r&&r<=1.0)
+            {
+                double nr = sqrt(1.0-r*r);
+                // new distance is between 0 ... 1
+                nr = (r + (1.0-nr)) / 2.0;
+                // discard radius greater than 1.0
+                if (nr<=1.0)
+                {
+                    // calculate the angle for polar coordinates
+                    double theta = atan2(ny,nx);
+                    // calculate new x position with new distance in same angle
+                    double nxn = nr*cos(theta);
+                    // calculate new y position with new distance in same angle
+                    double nyn = nr*sin(theta);
+                    // map from -1 ... 1 to image coordinates
+                    int x2 = (int)(((nxn+1)*w)/2.0);
+                    // map from -1 ... 1 to image coordinates
+                    int y2 = (int)(((nyn+1)*h)/2.0);
+                    // find (x2,y2) position from source pixels
+                    int srcpos = (int)(y2*w+x2);
+                    // make sure that position stays within arrays
+                    if (srcpos>=0 & srcpos < w*h)
+                    {
+                        // get new pixel (x2,y2) and put it to target array at (x,y)
+
+                        (uint32_t*)(pixels[(int)(y*w+x)]) = (uint32_t*)(pixels[srcpos]);
+                    }
+                }
+            }
         }
-        
-        pixels = (char*)pixels + info->stride;
     }
+
+    
+
 }
 
 
